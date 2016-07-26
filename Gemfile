1bdf54b8029e8832008fe9404c85a491e65443a2
keymastervn <vnkeymaster@gmail.com>
2016-07-26 14:52:34 +0700

    Change exampe type from mail to Height and Weight

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
 
 gem 'fuzzy-logic'
-gem 'gmail'
--- model/mail.rb
+++
@@ -1,9 +0,0 @@
-class Mail
-	attr_accessor :subject, :body, :from, :uid
-	def initialize(uid = nil, from = nil, subject = nil, body = nil)
-		@uid = uid
-		@from = from
-		@subject = subject
-		@body = body
-	end
-end
